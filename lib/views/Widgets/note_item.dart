9c6de6c033f39b2fe1feb150fc75a7f7da1e5333
Dev <devlabsstudio@gmail.com>
2023-08-19 17:22:17 -0700

    refresh notes list

--- lib/views/Widgets/note_item.dart
+++ lib/views/Widgets/note_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
@@ -52,7 +54,10 @@ class NotesItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                  },
                   icon: const Icon(
                     Icons.delete,
                     color: Colors.black,
